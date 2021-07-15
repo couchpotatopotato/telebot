08ce5aa5c23958edb2ebc8e6549dd295c0ebdebd
potato on the couch <nathanielgmh@gmail.com>
2021-07-16 01:04:12 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://chong-testbot.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
