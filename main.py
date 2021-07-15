13e030a6b2d4c00250fdc3e012e498e58cc5b56f
potato on the couch <nathanielgmh@gmail.com>
2021-07-16 00:44:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # using python-telegram-bot
-
+import os
 from telegram import *
 from telegram.ext import *
 
+PORT = int(os.environ.get('PORT', 5000))
 bot = Bot("1888950275:AAFX1jKq1kxhYhg2yfYW30k1WszssJijSIs")
 updater = Updater(
     "1888950275:AAFX1jKq1kxhYhg2yfYW30k1WszssJijSIs", use_context=True)
@@ -19,4 +20,7 @@ def test_function(update: Update, context: CallbackContext):
 
 start_value = CommandHandler("bye", test_function)
 dispatcher.add_handler(start_value)
-updater.start_polling()
+updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+updater.bot.setWebhook('https://chong-testbot.herokuapp.com/' + "1888950275:AAFX1jKq1kxhYhg2yfYW30k1WszssJijSIs")
