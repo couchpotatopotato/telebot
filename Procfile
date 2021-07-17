53720e449e8b7f20f92b945593873e6c57d4f35d
riccqi <xiaochean@gmail.com>
2021-07-17 23:23:22 +0800

    Add gunicorn

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python bot.py
+web: gunicorn wsgi:app
--- bot.py
+++ bot.py
@@ -36,8 +36,6 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-
-    app.run()
     updater = Updater(TOKEN, use_context=True)
 
     # Get the dispatcher to register handlers
