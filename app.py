7e77a795ba7007bab98b1c93a4884cd103906e58
zsiggg <57909679+zsiggg@users.noreply.github.com>
2021-07-17 20:25:21 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import *
+
+# creates the flask app
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    message = request.args.get('message')
+
+    # if there is no message submitted via HTML form yet, return None
+    if message is None:
+        return render_template('index.html')
+        
+    # else return acknowledgement that message is received
+    else:
+        print(message)
+        return 'Sent to telebot!'
