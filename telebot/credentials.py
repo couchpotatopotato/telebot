6067eac993aae0eda439009a1073288ab2409350
potato on the couch <nathanielgmh@gmail.com>
2021-07-20 00:23:27 +0800

    Update credentials.py

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
-bot_token = '1906815972:AAHziQSQdESMnC8fNRlkfJmrk3MUIp_mRFo'
-bot_user_name = 'thepotatopotatobot'
+bot_token = '1888950275:AAFX1jKq1kxhYhg2yfYW30k1WszssJijSIs'
+bot_user_name = 'potato_potato_bot'
 URL = 'https://chong-testbot2.herokuapp.com/'
