1d6a22794273e59ce3413785cba0854134b9f04a
potato on the couch <nathanielgmh@gmail.com>
2021-07-19 21:20:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from dotenv import load_dotenv
 
 PORT = int(os.environ.get('PORT', 5000))
-
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
@@ -34,46 +30,37 @@ def error(update, context):
 
 
 
-# creates the flask app
+    
+#FLASKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
 app = Flask(__name__)
 
+#TELEBOT
 @app.before_first_request
 def start_telebot():
     """Start the bot."""
-    # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
     updater = Updater(TOKEN, use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
+    updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
     updater.bot.setWebhook('https://chong-testbot2.herokuapp.com/' + TOKEN)
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
+    
 @app.route('/hello/', methods=['GET', 'POST'])
 def index():
     message = 'Test'
     # message = request.args.get('message')
-
     # if there is no message submitted via HTML form yet, return None
     if message is None:
         return render_template('index.html')
@@ -87,6 +74,5 @@ def index():
 def welcome():
     return "<h1>Welcome to THE CHONGSTERS server !!</h1>"
   
-  
 if __name__ == '__main__':
-   app.run(threaded=True)
+   app.run()
