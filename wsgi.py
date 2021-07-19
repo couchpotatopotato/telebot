a9c3877f2874e069ecedf4a5d4838446e8481ed6
potato on the couch <nathanielgmh@gmail.com>
2021-07-19 20:38:27 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
 from bot import main
 from app import app
-
-def run_please():
-        if __name__ == "__main__":
-                main()
-                app.run()
+        
+if __name__ == "__main__":
+        main()
+        app.run()
