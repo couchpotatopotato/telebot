c047e3127e736a4b8044177d6ae141c64ab28428
zsiggg <57909679+zsiggg@users.noreply.github.com>
2021-07-20 02:47:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def set_webhook():
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond(): 
     update = Update.de_json(request.get_json(force=True), bot)
-    print(dict(update))
+    print(list(update))
     # chat_id = update.message.chat.id
     # msg_id = update.message.message_id
 
