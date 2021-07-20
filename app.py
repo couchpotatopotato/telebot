0c8ff141cb13b52d6dfe7dd6c8efc986d2f9eedf
riccqi <xiaochean@gmail.com>
2021-07-20 13:31:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def set_webhook():
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond(): 
     update = Update.de_json(request.get_json(force=True), bot)
-    print(update.message + "--------------------------------------------------------")
+    print(update.message.text + "--------------------------------------------------------")
     # chat_id = update.message.chat.id
     # msg_id = update.message.message_id
 
