eeb3d562448057a801a0e78c7c4c4331fdda594c
potato on the couch <nathanielgmh@gmail.com>
2021-07-20 14:33:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,14 @@ from flask.templating import render_template
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Dispatcher
 from dotenv import load_dotenv
 from telegram.update import Update
-
 from flask import Flask, json, request
 from telegram import Bot
 from telebot.credentials import bot_token, bot_user_name,URL
-
 
 global bot
 global TOKEN
 TOKEN = bot_token
 bot = Bot(token=TOKEN)
-
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 load_dotenv()
@@ -28,48 +25,47 @@ dp = Dispatcher(bot, update_queue)
 def start_cmd(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi! I\'m created by the WANKSTERS. \n I will just repeat what you say OKAY')
-
 def help_cmd(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
-
 def echo_cmd(update, context):
     """Echo the user message."""
     update.message.reply_text(update.message.text)
-
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
     
     
-
 #FLASK APP
 app = Flask(__name__)
 
-def get_response():
-    dp.add_handler(CommandHandler("start", start_cmd))
-    dp.add_handler(CommandHandler("help", help_cmd))
-    dp.add_handler(MessageHandler(Filters.text, echo_cmd))
-    dp.add_error_handler(error)
-    thread = Thread(target=dp.start, name='dispatcher')
-    thread.start()
-
+@app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
     s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
     if s:
         return "webhook setup ok"
     else:
-        return "webhook setup failed" 
+        return "webhook setup failed"
 
+    
+def get_response(update):
+    dp.add_handler(CommandHandler("start", start_cmd))
+    dp.add_handler(CommandHandler("help", help_cmd))
+    dp.add_handler(MessageHandler(Filters.text, echo_cmd))
+    dp.add_error_handler(error)
+
+ 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
     update = Update.de_json(request.get_json(force=True), bot)
     update_queue.put(update)
+    response = get_response(update)
+    thread = Thread(target=dp.start, name='dispatcher')
+    thread.start()
     return 'ok'
     #chat_id = update.message.chat.id
     #msg_id = update.message.message_id
     #text = update.message.text.encode('utf-8').decode()
-    
 
 
 @app.route('/hello/', methods=['GET', 'POST'])
@@ -88,8 +84,5 @@ def index():
 def welcome():
     return "<h1>Welcome to THE CHONGSTERS server !!</h1>"
 
-
 if __name__ == '__main__':
-    get_response()
-    set_webhook()
-    app.run(threaded=True)
+   app.run(threaded=True)
