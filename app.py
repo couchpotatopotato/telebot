8365c859e8fb4d20aa268b6b90e695af16269376
zsiggg <57909679+zsiggg@users.noreply.github.com>
2021-07-23 21:49:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -143,6 +143,7 @@ def main():
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("subscribe", subscribe))
     dp.add_handler(CommandHandler("unsubscribe", unsubscribe))
+    dp.add_handler(CommandHandler("ask", ask))
 
     # log all errors
     dp.add_error_handler(error)
