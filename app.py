690f82a20b3af24e0ddc05d84d7b0bd30d464a60
zsiggg <57909679+zsiggg@users.noreply.github.com>
2021-07-24 02:18:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -65,16 +65,6 @@ def help(update, context):
 #     for row in cur.fetchall():
 #         print(row)
 
-def button(update: Update, context: CallbackContext) -> None:
-    """Parses the CallbackQuery and updates the message text."""
-    query = update.callback_query
-
-    # CallbackQueries need to be answered, even if no notification to the user is needed
-    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
-    query.answer()
-
-    query.edit_message_text(text=f"Selected option: {query.data}")
-
 
 def ask(update, context):
     print('----------ASK FUNCTION-------------')
