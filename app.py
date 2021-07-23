903b48d9e1d96de2b42f4edbf02dd2dc91fa448f
potato on the couch <nathanielgmh@gmail.com>
2021-07-23 22:30:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,22 +58,6 @@ def help(update, context):
     '''
     update.message.reply_text(mainmenu)
     
-    retrieved_data = []
-    cur.execute("""SELECT questions.question_id, questions.question_text, questions.question_answer, 
-    count(subscriptions.question_id) AS subscription_count 
-    FROM subscriptions
-    RIGHT JOIN questions on questions.question_id=subscriptions.question_id
-    GROUP BY question_id""")
-    for row in cur.fetchall():
-        dict = {}
-        dict["question_id"] = row[0]
-        dict["question_text"] = row[1]
-        dict["question_answer"] = row[2]
-        dict["subscription_count"] = row[3]
-        retrieved_data.append(dict)
-    json_data = json.dumps(retrieved_data)
-    print(json_data)
-
 # def echo(update, context):
 #     """Echo the user message."""
 #     print('-----ECHO FUNCTION-----')
@@ -100,6 +84,24 @@ def ask_sendquestion(update, context):
 def ask_getquestion(update, context):
     print('-----getting the question-------')
     update.message.reply_text('ok')
+    
+    
+    retrieved_data = []
+    cur.execute("""SELECT questions.question_id, questions.question_text, questions.question_answer, 
+    count(subscriptions.question_id) AS subscription_count 
+    FROM subscriptions
+    RIGHT JOIN questions on questions.question_id=subscriptions.question_id
+    GROUP BY question_id""")
+    for row in cur.fetchall():
+        dict = {}
+        dict["question_id"] = row[0]
+        dict["question_text"] = row[1]
+        dict["question_answer"] = row[2]
+        dict["subscription_count"] = row[3]
+        retrieved_data.append(dict)
+    json_data = json.dumps(retrieved_data)
+    print(json_data)
+
 
     # # ensure that there is a question
     # question = update.message.text
