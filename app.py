cacc3ccf88602a98fa838a32b5ca1dc1b71fdb12
potato on the couch <nathanielgmh@gmail.com>
2021-07-23 23:55:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask.templating import render_template
 from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler, ConversationHandler, Filters, Dispatcher, callbackqueryhandler, dispatcher
 from dotenv import load_dotenv
 from telegram.update import Update
+from flask_cors import CORS, cross_origin
 
 from flask import Flask, json, request
 from telegram import Bot
@@ -21,9 +22,8 @@ bot = Bot(token=TOKEN)
 update_queue = Queue()
 dp = Dispatcher(bot, update_queue)
 GET_QUESTION, SEND_QUESTION = range(2)
-
 # create dictionary storing chat_id and group title/username key-value pairs
-SUBSCRIPTION_CHAT_ID_TO_USERNAME = {} 
+SUBSCRIPTION_CHAT_ID_TO_USERNAME = {}
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -33,19 +33,26 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 
 # connect to the database
-conn = mysql.connector.connect(user='bb75a740c4787a', password='6ae814c8', host='us-cdbr-east-04.cleardb.com', database='heroku_aff68423aab93c1', connect_timeout=28800)
+conn = mysql.connector.connect(user='bb75a740c4787a', password='6ae814c8',
+                               host='us-cdbr-east-04.cleardb.com', database='heroku_aff68423aab93c1', connect_timeout=1000)
 print('conn done')
 cur = conn.cursor()
 print('cur done')
+conn.query('SET GLOBAL connect_timeout=28800')
+conn.query('SET GLOBAL interactive_timeout=28800')
+conn.query('SET GLOBAL wait_timeout=28800')
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
+
+
 def start(update, context):
     """Send a message and show the main menu when the command /start is issued."""
     print('-----START FUNCTION-----')
     remove_unneeded_handlers()
     update.message.reply_text('Welcome to the chongsters bot!')
     help(update, context)  # to show the main menu
+
 
 def help(update, context):
     """Send the main menu when the command /help is issued."""
@@ -64,7 +71,7 @@ def help(update, context):
 #     """Echo the user message."""
 #     print('-----ECHO FUNCTION-----')
 #     update.message.reply_text(update.message.text)
-  
+
 #     cur.execute('INSERT INTO questions (question_text, question_answer) VALUES (%s, %s)', (update.message.text, 'no answer yet'))
 #     print('insert done')
 
@@ -72,11 +79,13 @@ def help(update, context):
 #     for row in cur.fetchall():
 #         print(row)
 
+
 def ask(update, context):
     print('----------ASK FUNCTION-------------')
     remove_unneeded_handlers()
     update.message.reply_text('What is your question?')
-    return GET_QUESTION
+    dp.add_handler(MessageHandler(Filters.text, ask_getquestion))
+
 
 def ask_getquestion(update, context):
     print('-----getting the question-------')
@@ -91,21 +100,26 @@ def ask_getquestion(update, context):
         # process through NLP
         # if repetitive, prompt the user and suggest that they subscribe to the other question
 
-        options_yesno = {'inline_keyboard':[[{'text': 'Yes', 'callback_data': update.message.text}], [{'text': 'No', 'callback_data': '0'}]]}
-        bot.sendMessage(chat_id=update.message.chat.id, text='Your question is "' + update.message.text + '". Send this to the presenter?', reply_markup=options_yesno)
+        options_yesno = {'inline_keyboard': [
+            [{'text': 'Yes'}], [{'text': 'No'}]]}
+        bot.sendMessage(chat_id=update.message.chat.id, text='Your question is "' +
+                        update.message.text + '". Send this to the presenter?', reply_markup=options_yesno)
 
-        return SEND_QUESTION
+        dp.add_handler(MessageHandler(Filters.text, ask_sendquestion))
+
 
 def ask_sendquestion(update, context):
     print('----------sending the question-----------')
     remove_unneeded_handlers()
 
-    if update.callback_query.data != '0':
-        cur.execute('INSERT INTO questions (question_text) VALUES (%s)', (update.callback_query.data))
+    if update.message.text != '0':
+        cur.execute('INSERT INTO questions (question_text) VALUES (%s)',
+                    (update.callback_query.data))
         # check for errors
         update.message.reply_text('Your message has been added!')
     else:
         ask(update)
+
 
 def error(update, context):
     """Log Errors caused by Updates."""
@@ -113,16 +127,21 @@ def error(update, context):
     remove_unneeded_handlers()
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
+
 def subscribe(update, context):
     """Add users to subscription list to allow sending of messages later"""
     remove_unneeded_handlers()
     if not SUBSCRIPTION_CHAT_ID_TO_USERNAME.get(update.message.chat.id, False):
         # store group title/username for groups/private chat respectively as the value
-        SUBSCRIPTION_CHAT_ID_TO_USERNAME[update.message.chat.id] = update.message.chat.title if update.message.chat.type == 'group' else '@' + update.message.from_user.username
-        update.message.reply_text(SUBSCRIPTION_CHAT_ID_TO_USERNAME[update.message.chat.id] + ' has been added to the subscription list!')
+        SUBSCRIPTION_CHAT_ID_TO_USERNAME[update.message.chat.id] = update.message.chat.title if update.message.chat.type == 'group' else '@' + \
+            update.message.from_user.username
+        update.message.reply_text(
+            SUBSCRIPTION_CHAT_ID_TO_USERNAME[update.message.chat.id] + ' has been added to the subscription list!')
 
     else:
-        update.message.reply_text(SUBSCRIPTION_CHAT_ID_TO_USERNAME[update.message.chat.id] + ' is already in the subscription list!')
+        update.message.reply_text(
+            SUBSCRIPTION_CHAT_ID_TO_USERNAME[update.message.chat.id] + ' is already in the subscription list!')
+
 
 def unsubscribe(update, context):
     """Remove user from subscription list"""
@@ -135,16 +154,23 @@ def unsubscribe(update, context):
 
     if not SUBSCRIPTION_CHAT_ID_TO_USERNAME.pop(update.message.chat.id, False):
         # return group name / username not in subscription list
-        update.message.reply_text(username_or_group + ' is not in the subscription list!')
+        update.message.reply_text(
+            username_or_group + ' is not in the subscription list!')
     else:
-        update.message.reply_text(username_or_group + ' has been removed from the subscription list!')
+        update.message.reply_text(
+            username_or_group + ' has been removed from the subscription list!')
+
 
 def remove_unneeded_handlers():
     dp.remove_handler(MessageHandler(Filters.text, ask_getquestion))
-    dp.remove_handler(CallbackQueryHandler(callback=ask_sendquestion))
+    dp.remove_handler(MessageHandler(Filters.text, ask_sendquestion))
+
 
 # creates the flask app
 app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
+
 
 @app.before_first_request
 def main():
@@ -153,14 +179,7 @@ def main():
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("subscribe", subscribe))
     dp.add_handler(CommandHandler("unsubscribe", unsubscribe))
-    dp.add_handler(ConversationHandler(
-        entry_points=[CommandHandler("ask", ask)],
-        states={GET_QUESTION: [MessageHandler(Filters.text, ask_getquestion)],
-                SEND_QUESTION: [CallbackQueryHandler(callback=ask_sendquestion)]
-        },
-        fallbacks=[]
-    ))
-    
+    dp.add_handler(CommandHandler("ask", ask))
 
     # log all errors
     dp.add_error_handler(error)
@@ -176,14 +195,17 @@ def main():
     print("---------------------------------------------webhook STARTED----------------------------------------------")
 
 # processing requests made by user from telebot
+
+
 @app.route('/{}'.format(TOKEN), methods=['POST'])
-def respond(): 
+def respond():
     update = Update.de_json(request.get_json(), bot)
 
     if update.message == None:
         return 'bad update'
 
-    print(update.message.text + "--------------------------------------------------------")
+    print(update.message.text +
+          "--------------------------------------------------------")
     # Telegram understands UTF-8, so encode text for unicode compatibility
     # text = update.message.text.encode('utf-8').decode()
 
@@ -191,38 +213,40 @@ def respond():
 
     return 'good update'
 
+
 @app.route('/sendmessage')
 def sendmessage():
     message = request.args.get('message')
-    
+
     # send the message to everyone in the subscription list
-    if len(SUBSCRIPTION_CHAT_ID_TO_USERNAME) == 0 :
+    if len(SUBSCRIPTION_CHAT_ID_TO_USERNAME) == 0:
         return 'no one has subscribed to the bot yet'
     else:
         for chat_id in SUBSCRIPTION_CHAT_ID_TO_USERNAME:
             bot.sendMessage(chat_id=chat_id, text=message)
-        
+
         # print the subscription list after sending messages
         subscriptionlist = '\n'.join(SUBSCRIPTION_CHAT_ID_TO_USERNAME.values())
         return 'sent to ' + str(len(SUBSCRIPTION_CHAT_ID_TO_USERNAME)) + ' persons/groups\n' + subscriptionlist
-    
 
-        
+
 @app.route('/')
 def welcome():
     return "<h1>Welcome to THE CHONGSTERS server!!</h1>"
 
-  
-@app.route('/answer', methods=['GET','POST'])
+
+@app.route('/answer', methods=['POST'])
 @cross_origin()
 def answer():
     input_json = request.get_json(force=True)
     answer_question_text = input_json["answer"]
     question_id = input_json["id"]
-    cur.execute('UPDATE questions SET question_answer= %s WHERE question_id= %s', (answer_question_text, question_id))
-    return "ok"
+    cur.execute('UPDATE questions SET question_answer= %s WHERE question_id= %s',
+                (answer_question_text, question_id))
+
 
 @app.route('/retrieve', methods=['GET', 'POST'])
+@cross_origin()
 def retrieve():
     retrieved_data = []
     cur.execute("""SELECT questions.question_id, questions.question_text, questions.question_answer, 
@@ -239,6 +263,7 @@ def retrieve():
         retrieved_data.append(dict)
     json_data = json.dumps(retrieved_data)
     return json_data
-  
+
+
 if __name__ == '__main__':
     app.run(threaded=True)
